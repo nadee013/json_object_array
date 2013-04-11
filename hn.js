92520541e2d06e0265396672d7ffac73d868d583
Nadee Anuththara <nadee013@gmail.com>
2013-04-11 23:46:06 +0530

    Done error handling

--- hn.js
+++ hn.js
@@ -2,8 +2,12 @@ var request = require("request");
 
 module.exports.getHomePage = function(callback) {
 
-	request('http://hndroidapi.appspot.com/news/format/json/page/?appid=test-app', function (error, response, body) {
-		if (!error && response.statusCode == 200) {
+	var url = 'http://hndroidapi.appspot.com/news/format/json/page/?appid=test-app';
+	
+	request(url , function (error, response, body) {
+		if(error) {
+			callback(error);
+		} else if (response.statusCode == 200) {
 			if(typeof(body) == "string") {
 				var js_object = JSON.parse(body);
 				var object_array = js_object.items;
