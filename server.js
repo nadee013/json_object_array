def723f1c7c1ca1ccc54f3adbaaf2d25794f58dc
Nadee Anuththara <nadee013@gmail.com>
2013-04-11 17:36:16 +0530

    Added a hn module

--- server.js
+++ server.js
@@ -1,19 +1,5 @@
-var request = require("request");
+var hn = require('./hn');
 
-// console.log("making request");
-
-request('http://hndroidapi.appspot.com/news/format/json/page/?appid=test-app', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    if(typeof(body) == "string") {
-    	var js_object = JSON.parse(body);
-    	// console.log(js_object.items);
-    	var object_array = js_object.items;
-    	console.log(object_array.length);
-    	for(var i = 0; i < object_array.length; i++) {
-    		console.log("Title " +i+ "is " +object_array[i].title);
-    	}
-    }
-   	// console.log(body); // Print the string object
-
-  }
-})
+hn.getHomePage(function(err, titles) {
+  console.log(titles);
+});
